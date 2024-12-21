e73c5690ee7545a2264beae5004bcbc428e48e12
jparep <parepson@gmail.com>
2024-12-21 14:30:28 -0700

    Requirements created

--- visualizations.py
+++ visualizations.py
@@ -1,7 +1,7 @@
 import plotly.express as px
 
 def create_balance_histogram(data):
-    return px.histogram(data, x='Balance', nbins=20, title='Balance Distribution')
+    return px.histogram(data, x="Balance", nbins=20, title="Balance Distribution")
 
 def create_loan_status_pie(data):
-    return px.pie(data, names='Loan_Status', title='Loan Status Distribution')
+    return px.pie(data, names="Loan_Status", title="Loan Status Distribution")
