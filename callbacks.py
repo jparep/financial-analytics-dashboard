3b901578ff1889725fd5301c755f9eab0f4f46ff
jparep <parepson@gmail.com>
2024-12-21 14:10:32 -0700

    Update callbacks file

---
+++ callbacks.py
@@ -0,0 +1,16 @@
+from dash.dependencies import Input, Output
+from data_loader import load_filtered_data
+from visualizations import create_balance_histogram, create_loan_status_pie
+
+def register_callbacks(app):
+    @app.callback(
+        [Output('balance_distribution', 'figure'),
+         Output('loan_status_chart', 'figure')],
+        [Input('region_selector', 'value'),
+         Input('account_selector', 'value')]
+    )
+    def update_graphs(selected_region, selected_account):
+        filtered_data = load_filtered_data(selected_region, selected_account)
+        balance_fig = create_balance_histogram(filtered_data)
+        loan_fig = create_loan_status_pie(filtered_data)
+        return balance_fig, loan_fig
