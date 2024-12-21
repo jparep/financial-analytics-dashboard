d03b822017154465078c27a893d4917fede3f6b4
jparep <parepson@gmail.com>
2024-12-21 14:21:59 -0700

    Dataloader updated

--- data_loader.py
+++ data_loader.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
 def load_filtered_data(region, account_type):
-    df = pd.read_csv('synthetic_banking_data.csv')
-    return df[(df['Region'] == region) & (df['Account_Type'] == account_type)]
+    # Load the dataset
+    df = pd.read_csv("synthetic_banking_data.csv")
+    # Filter data based on the selected region and account type
+    return df[(df["Region"] == region) & (df["Account_Type"] == account_type)]
