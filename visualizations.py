8b1a6cb571890fe06ac7a5fab1c23f5aea08a761
jparep <parepson@gmail.com>
2024-12-22 14:46:35 -0700

    Updated visualization to show monthly transaction

--- visualizations.py
+++ visualizations.py
@@ -5,3 +5,13 @@ def create_balance_histogram(data):
 
 def create_loan_status_pie(data):
     return px.pie(data, names="Loan_Status", title="Loan Status Distribution")
+
+def create_monthly_transaction_trend(data):
+    return px.line(
+        data, 
+        x="Month", 
+        y="Transaction_Amount", 
+        title="Monthly Transaction Trends", 
+        markers=True,
+        labels={"Transaction_Amount": "Total Transactions"}
+    )
