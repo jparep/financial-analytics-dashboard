170ad8009860ae40d06b29bd7de6abf48f760ac3
jparep <parepson@gmail.com>
2024-12-22 12:05:54 -0700

    Updated layout

--- layout.py
+++ layout.py
@@ -3,12 +3,11 @@ import pandas as pd
 
 def create_layout():
     # Load a sample of the dataset to populate dropdown options
-    df = pd.read_csv("./data/banking_data.csv")
+    df = pd.read_csv("synthetic_banking_data.csv")
 
     return html.Div([
         html.H1("Banking Data Dashboard", style={"text-align": "center"}),
 
-        # Dropdown Filters
         html.Div([
             dcc.Dropdown(
                 id="region_selector",
@@ -24,17 +23,8 @@ def create_layout():
                 multi=False,
                 style={"width": "50%"}
             )
-        ], style={"display": "flex", "justify-content": "space-between"}),
+        ]),
 
-        # Row 1: Balance Distribution and Loan Status Charts
-        html.Div([
-            dcc.Graph(id="balance_distribution", style={"width": "48%", "display": "inline-block"}),
-            dcc.Graph(id="loan_status_chart", style={"width": "48%", "display": "inline-block"})
-        ], style={"display": "flex", "justify-content": "space-between"}),
-
-        # Row 2: Monthly Transactions Trend and Customer Segmentation by Region
-        html.Div([
-            dcc.Graph(id="monthly_transactions_trend", style={"width": "48%", "display": "inline-block"}),
-            dcc.Graph(id="customer_segmentation", style={"width": "48%", "display": "inline-block"})
-        ], style={"display": "flex", "justify-content": "space-between"})
+        dcc.Graph(id="balance_distribution"),
+        dcc.Graph(id="loan_status_chart")
     ])
